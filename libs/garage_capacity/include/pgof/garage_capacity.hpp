cfe7e6d6e6f74960585e32e6aa3993dd1fe22aba
loumcnamee <mcnamee.lou@gmail.com>
2026-05-28 19:18:32 -0400

    Add garage_capacity source code

---
+++ libs/garage_capacity/include/pgof/garage_capacity.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace pgof::garage {
+
+// Represents the number of spaces in the garage of each size 
+struct garage_capacity {
+    int n1; // Number of spaces in the first bucket (size 1)
+    int n2; // Number of spaces in the second bucket (size 2)
+    int n3; // Number of spaces in the third bucket (size 3)
+};
+
+// Splits N spaces into size 1/2/3 buckets as evenly as possible.
+garage_capacity get_garage_capacity(int total_spaces);
+
+}  // namespace pgof::garage
---
+++ libs/garage_capacity/src/garage_capacity.cpp
@@ -0,0 +1,28 @@
+#include "pgof/garage_capacity.hpp"
+
+#include <stdexcept>
+
+namespace pgof::garage {
+
+namespace {
+constexpr int min_spaces = 10;
+constexpr int max_spaces = 1000000;
+}
+
+garage_capacity get_garage_capacity(const int total_spaces) {
+    if (total_spaces < min_spaces || total_spaces > max_spaces) {
+        throw std::out_of_range("total_spaces must be in [10, 1000000]");
+    }
+
+    const int base = total_spaces / 3;
+    const int remainder = total_spaces % 3;
+
+    garage_capacity capacity{};
+    capacity.n1 = base + (remainder > 0 ? 1 : 0);
+    capacity.n2 = base + (remainder > 1 ? 1 : 0);
+    capacity.n3 = base;
+
+    return capacity;
+}
+
+}  // namespace pgof::garage
